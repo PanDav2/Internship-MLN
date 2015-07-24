66a173207d0405aea4f537590b9bcd9aeb44abda
David <david.panou@gmail.com>
2015-07-24 17:31:23 +0200

    Ajout de SCNLP

---
+++ Information_extraction/stanford_nlp_main.py
@@ -0,0 +1,5 @@
+### This file is made in the aim of testing Stanford NLP Information Extraction System in order to compare it to the syntactic augmented IE method using Markov Logic Network ###
+
+# -*- coding: utf-8 -*-
+
+from gensim
