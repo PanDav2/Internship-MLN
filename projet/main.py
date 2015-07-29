657ef8a705997f07ef63393c6955212e07a95bf2
David <david.panou@gmail.com>
2015-07-29 19:42:19 +0200

    extraction de l'ensemble des questions du corpus

--- projet/main.py
+++ projet/main.py
@@ -8,20 +8,24 @@ import numpy as np
 
 loc = "./donnee.xml"
 data = "./data.txt"
-quest = "./quest.txt"
+quest = "./quest-en.txt"
+
+lan=["fr,en,es,it,fr,nl,ro"]
 
 with open(data,'w') as out:
     with open(loc,'r') as inp:
         for line in inp:
             #On détecte les lignes ou les phrases sont en allemand
-            if "<string lang=" in line:
+            if "<string lang=\"en" in line:
                 out.write(inp.next())
 
 with open(data,'r') as inp:
     with open(quest,'w') as out:
         for line in inp:
             if len(line.split())>1:
+                """
                 #Tentative de concatenner l'ensemble des mots en une phrase
                 for i in len(line.split()):
                     print line.split()[i]
-                #out.write(line[1:len(line)-2])
+                """
+                out.write(line[10:len(line)-4]+'\n')
