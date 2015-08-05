01e3544fd9f3d73acb678999620c08bda691e97c
David <david.panou@gmail.com>
2015-08-05 16:31:37 +0200

    Minor modifications

--- lancement.sh
+++ lancement.sh
@@ -1,8 +1,6 @@
-#!/bin/bash
-export PYTHONPATH=$(cat ./PYTHONPATH)
+echo export PYTHONPATH=$(cat ./PYTHONPATH)
 
-echo Le PYTHONPATH a bien été modifié.
+echo export PATH=/people/panou/Stage/Java/jdk1.8.0_51/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games
 
-export PATH=/people/panou/Stage/Java/jdk1.8.0_51/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games
 
-echo Le répertoire de JAVA a bien été ajouté aux PATH.
+echo '#### COPIER LES LIGNES SUIVANTES ET LES LANCER DANS LE TERMINAL ####'
--- projet/main.py
+++ projet/main.py
@@ -5,27 +5,35 @@
 
 import re
 import numpy as np
+from stanford_corenlp_pywrapper import CoreNLP
 
-loc = "./donnee.xml"
-data = "./data.txt"
-quest = "./quest-en.txt"
 
-lan=["fr,en,es,it,fr,nl,ro"]
-
-with open(data,'w') as out:
-    with open(loc,'r') as inp:
-        for line in inp:
+#TODO extraire a partir d'un fichier donnée en argument (adresse, puis interface graphique) 
+def extract():
+    loc = "./donnee.xml"
+    data = "./data.txt"
+    quest = "./quest-en.txt"
+    #TODO gérer l'extraction multi-lingue par un tableau
+    lan=["fr,en,es,it,fr,nl,ro"]
+    
+    with open(data,'w') as out:
+        with open(loc,'r') as inp:
+            for line in inp:
             #On détecte les lignes ou les phrases sont en allemand
-            if "<string lang=\"en" in line:
-                out.write(inp.next())
+                if "<string lang=\"en" in line:
+                    out.write(inp.next())
 
-with open(data,'r') as inp:
-    with open(quest,'w') as out:
-        for line in inp:
-            if len(line.split())>1:
-                """
-                #Tentative de concatenner l'ensemble des mots en une phrase
-                for i in len(line.split()):
+    with open(data,'r') as inp:
+        with open(quest,'w') as out:
+            for line in inp:
+                if len(line.split())>1:
+                    """
+                    #Tentative de concatenner l'ensemble des mots en une phrase
+                    for i in len(line.split()):
                     print line.split()[i]
-                """
-                out.write(line[10:len(line)-4]+'\n')
+                    """
+                    out.write(line[10:len(line)-4]+'\n')
+
+#POS tagging using Stanford NLP tagger
+
+proc = CoreNLP("pos",corenlp_jars=["/people/panou/Stage/projet/stanford-corenlp-full-2015-04-20/*"])
